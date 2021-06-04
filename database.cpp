3d4f5dda001559534b14e62be79b40ddcd13eb27
astroidis <69013280+astroidis@users.noreply.github.com>
2021-06-04 11:15:39 +0300

    show messagebox with error text on failure

--- database.cpp
+++ database.cpp
@@ -1,5 +1,5 @@
 #include "database.h"
-#include <QDebug>
+#include <QMessageBox>
 
 Database::Database(QObject *parent) : QObject(parent)
 {
@@ -8,6 +8,13 @@ Database::Database(QObject *parent) : QObject(parent)
 void Database::makeConnection()
 {
     conn = QSqlDatabase::addDatabase("QPSQL", "stokes_db");
+    if (!conn.isValid()){
+        QMessageBox msgb;
+        msgb.setText(conn.lastError().text());
+        msgb.setStandardButtons(QMessageBox::Cancel);
+        msgb.exec();
+    }
+
     conn.setDatabaseName("stokes_db");
     conn.setHostName("127.0.0.1");
     conn.setPort(5432);
@@ -19,8 +26,10 @@ bool Database::openDB()
 {
     if (!conn.isOpen()){
         if (!conn.open()){
-            qDebug() << conn.lastError();
-            return false;
+            QMessageBox msgb;
+            msgb.setText(conn.lastError().text());
+            msgb.setStandardButtons(QMessageBox::Cancel);
+            msgb.exec();
         }
     }
     return true;
